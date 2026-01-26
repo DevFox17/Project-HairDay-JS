badbc55b5b0844388df4fb6fd24d75a71f9cb377
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-26 20:25:19 -0300

    base formating for hours of operation in project with js

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs"
+import {openingHours} from "../../utils/opening-hours.js"
+
+export function hoursLoad( {date } ) {
+  const opening = openingHours.map((hour) => {
+    // Recupera somente a hora
+    const [schedulesHour] = hour.split(":")
+
+    // Adiciona a hora na data e verificar se está no passado
+    const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs())
+
+    return {
+      hour,
+      avaiable: isHourPast,
+    }
+  })
+}
---
+++ src/modules/schedules/load.js
@@ -0,0 +1,17 @@
+// Carregar agendamento do dia
+
+import {hoursLoad} from "../form/hours-load.js"
+
+// Seleciona o input de data
+const selectDate = document.getElementById("date")
+export function schedulesDay () {
+  // Obtém a data do input
+  const date = selectDate.value
+
+  // Renderiza as horas dsponíveis
+  hoursLoad( {date} )
+
+  // Busca na API os agendamentos para carregar do lado direito da tela
+
+  // Os horários disponíveis (horário futuro + não agendado) do lado esquerdo (from)
+}
