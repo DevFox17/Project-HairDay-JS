207a824217772dd3e0a2aa18517ce0470839f976
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-27 14:48:54 -0300

    add event of click in hours

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,32 +1,55 @@
 import dayjs from "dayjs"
 
 import {openingHours} from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad( { date } ) {
+export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
-    const [schedulesHour] = hour.split(":")
+    const [scheduleHour] = hour.split(":")
 
     // Adiciona a hora na data e verificar se está no passado
-    const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
 
     return {
       hour,
-      avaiable: isHourPast,
+      available: isHourPast,
     }
   })
 
   // Renderizar os horários
-  opening.forEach(({hour, available}) => {
+  opening.forEach(({ hour, available }) => {
     const li = document.createElement("li")
 
     li.classList.add("hour")
     li.classList.add(available ? "hour-available" : "hour-unavailable")
 
     li.textContent = hour
+
+    // Antes de adicionar o item na ul, verifica se tem um cabeçalho pra colocar 
+    if(hour ==="9:00") {
+      hourHeaderAdd("Manhã")
+    }
+    else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    }
+    else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
+
     hours.append(li)
   })
 
+  // Adiciona o evento de click nos horários disponíveis
+  hoursClick()
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+
+  hours.append(header)
 }
--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,5 +1,5 @@
-import {schedulesDay} from "./schedules/load.js"
+import {scheduleDay} from "./schedules/load.js"
 
 document.addEventListener("DOMContentLoaded", function() {
- schedulesDay()
+ scheduleDay()
 })
