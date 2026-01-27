839d697f008c4f1e0f553c5212ac4caa7c03f50b
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-27 15:21:57 -0300

    config appoitments and selected hour

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,12 +1,13 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Date atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 
-// Carrega a data atual e define a data mínima como sento a data atual
+// Carrega a data atual e define a data mínima como sendo a data atual
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
@@ -14,5 +15,38 @@ form.onsubmit = (event) => {
   // Previne o comportamento padrão de carregar a página
   event.preventDefault()
 
-  console.log("ENVIADO!")
+  try {
+    // Recuperando o nome do cliente
+    const name = clientName.value.trim()
+
+    if (!name) {
+      return alert("Informe o nome do cliente!")
+    }
+
+
+    // Recupera o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected")
+
+    // Recupera o horário selecionado
+    if (!hourSelected) {
+      return alert("Selecione a hora")
+    }
+
+    // Recuperar somente a hora
+    const hour = Number(hourSelected.innerText.split(":")[0])
+
+    // Insere a hora na data
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    // Gera um ID
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name,
+      when,
+    })
+  } catch (error) {
+    alert("não foi possível realizar o agendamento")
+  }
 }
