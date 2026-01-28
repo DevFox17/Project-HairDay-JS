252a5f9881fc61100d6466339c0378602df1d7c7
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-28 00:12:06 -0300

    Fetches the scheduled items from the API to load on the right side of the screen

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,17 +1,21 @@
 // Carregar agendamento do dia
-
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import {hoursLoad} from "../form/hours-load.js"
 
 // Seleciona o input de data
 const selectDate = document.getElementById("date")
-export function scheduleDay() {
+export async function scheduleDay() {
   // Obtém a data do input
   const date = selectDate.value
+
+  // Busca na API os agendamentos para carregar do lado direito da tela
+  const dailySchedules = await scheduleFetchByDay({ date })
+  console.log(dailySchedules)
 
   // Renderiza as horas dsponíveis
   hoursLoad( {date} )
 
-  // Busca na API os agendamentos para carregar do lado direito da tela
+  
 
   // Os horários disponíveis (horário futuro + não agendado) do lado esquerdo (from)
 }
