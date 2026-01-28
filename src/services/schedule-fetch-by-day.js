2bdcc6914169225a6ab22e0d691f5696c43c9dba
Jandavi <rodriguesjandavi193@gmail.com>
2026-01-27 23:54:40 -0300

    search appoitments of api

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,22 @@
+import dayjs from "dayjs"
+import { apiConfig } from "./api-config"
+
+export async function scheduleFetchByDay({ date }) {
+  try {
+    // Buscando/Fazendo a requisição dos dados na API
+    const response = await fetch(`${apiConfig.baseURL}/schedules`)
+
+    // Converte para JSON
+    const data = await response.json()
+
+    // Filtra os agendamentos pelo dia selecionado
+    const dalySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, "day")
+    )
+
+    return dalySchedules
+
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível buscar os agendamentos do dia selecionado.")
+  }
+}
